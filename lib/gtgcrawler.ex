a8ac27061cd9899979d80d5bd2b20fbb48c1a60e
Ilkka Poutanen <ilkka@ilkka.dev>
2023-10-26 09:29:10 +0300

    Initial commit

---
+++ lib/gtgcrawler.ex
@@ -0,0 +1,18 @@
+defmodule Gtgcrawler do
+  @moduledoc """
+  Documentation for `Gtgcrawler`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Gtgcrawler.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/gtgcrawler_test.exs
@@ -0,0 +1,8 @@
+defmodule GtgcrawlerTest do
+  use ExUnit.Case
+  doctest Gtgcrawler
+
+  test "greets the world" do
+    assert Gtgcrawler.hello() == :world
+  end
+end
