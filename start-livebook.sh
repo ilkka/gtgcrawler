88ebde25e982fb5609c3c415557f2e807687211d
Ilkka Poutanen <ilkka@ilkka.dev>
2023-10-27 21:53:17 +0300

    Actually fix the livebook script

--- start-livebook.sh
+++ start-livebook.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-docker run -p 8080:8080 -p 8081:8081 --pull always -u "$(id -u):$(id -g)" -v "$(pwd):/data ghcr.io/livebook-dev/livebook"
+docker run -p 8080:8080 -p 8081:8081 --pull always -u "$(id -u):$(id -g)" -v "$(pwd):/data" ghcr.io/livebook-dev/livebook
